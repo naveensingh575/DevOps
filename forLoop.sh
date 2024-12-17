c0d5e89ba232b8bcfa715a85ee4c68d724789a5b
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2024-12-17 17:35:00 +0530

    Update forLoop.sh

--- forLoop.sh
+++ forLoop.sh
@@ -9,3 +9,19 @@ for file in ${files[@]}
 do
         echo $file
 done
+
+#to iterate over a list of number
+for num in {0..10}
+do
+        echo "Creating VM $num"
+done
+
+
+#to iterate over a list of number
+#echo "Please input the number"
+#read number
+#echo $number
+#for num in {0..$number}
+#do
+#        echo "Creating VM $num"
+#done
