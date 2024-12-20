c4fe71175b4fae5e140c6cc08e33a78bcd99ddb9
Naveen KUmar <naveenofficial210@gmail.com>
2024-12-21 03:18:52 +0530

    add

--- Python/ticTac.py
+++ Python/ticTac.py
@@ -1,5 +1,15 @@
 #we will create the game in python
-value = input("please tell X or O : " )
-position = input("please tell the position :")
-print(value)
-print(position)
+ticTac = ["","","","","","","","",""]
+i = 1
+while i <= 9:
+    value = input("please tell X or O : " )
+    position = input("please tell the position :")
+    print(value)
+    print(position)
+    if ticTac[position] != "":
+        if value == "X" or  value == "O":
+            ticTac[position] = value
+        else:
+            Print(value + "is the invalid input")
+    else:
+        print("the value already exist")
