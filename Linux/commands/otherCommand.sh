7bc06c2a36269c99a1c68b02142979544101cdf9
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2024-12-20 13:09:24 +0530

    Create otherCommand.sh

---
+++ Linux/commands/otherCommand.sh
@@ -0,0 +1,9 @@
+get know more about commands
+whatis command - short and descriptive what command do.
+coommand --help - full description and other option
+man command     - documenration of command
+tab key will complete the command , direcrory name or file name.
+  sq<double tab> list all commands with sq like sqlplus and other.
+  ls c<tab> - compelte the directory or file name and doble tab list the name or directory or files start with c.
+uparrow key - give the previous command executed
+history - give the list of all previous commands executed.
