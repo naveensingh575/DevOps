1c47d70c5617251c352f0228bac70484b713a272
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2024-12-20 12:52:52 +0530

    Update switchCase1.sh

--- switchCase1.sh
+++ switchCase1.sh
@@ -19,23 +19,28 @@ for e.g
 echo "Input the type of dice"
 read dice
 case $dice in
-        "Tetrahedron" )
+       Tetrahedron )
                 echo "$dice  is 4 face dice" ;;
-        "Cube" )
+        Cube )
                 echo "$dice is 6 face dice" ;;
-        "Octahedron" )
+        Octahedron )
                 echo "$dice is 8 face dice" ;;
         "Pentagonal trapezohedron" )
                 echo "$dice is 10 face dice" ;;
-        "Dodecahedron" )
+        Dodecahedron )
                 echo "$dice is 12 face dice" ;;
-        "Icosahedron" )
+        Icosahedron )
                 echo "$dice is 20 face dice" ;;
 
-        *)
+         *)
                 echo "$dice is a invalid dice" ;;
-esac    
+esac
+
 
 #learning
 read variable -variable without $
-"Tetrahedron" - string in "" withot "" giving fatal
+"Pentagonal trapezohedron" - white space not allowed in pattern without "". it will give fatal giving fatal
+Pentagonal trapezohedron ) - fatal
+
+./rollADice.sh: line 11: syntax error near unexpected token `trapezohedron'
+./rollADice.sh: line 11: `      Pentagonal trapezohedron )'
