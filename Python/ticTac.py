8c1d525941c223277a77fe10bceded96cf8afe29
Naveen KUmar <naveenofficial210@gmail.com>
2024-12-21 16:02:09 +0530

    TictacToe

--- Python/ticTac.py
+++ Python/ticTac.py
@@ -7,16 +7,16 @@ while i <= 9:
     print(value)
     #print(int(position))
     print(int(int(position) -1))
-    if position >= 1 or position <= 9:
+    if int(position) >= 1 or int(position) <= 9:
         if ticTac[int(int(position) -1)] == "":
             if value == "X" or  value == "O":
                 ticTac[int(int(position) -1)] = value
                 print(ticTac[int(int(position) -1)])
-
+                print(ticTac)
                 if ((ticTac[0] == ticTac[1]) and (ticTac[0] == ticTac[2]) and ticTac[0] != "" ) or  ((ticTac[0] == ticTac[3]) and (ticTac[0] == ticTac[6]) and ticTac[0] != "" ) or  ((ticTac[0] == ticTac[4]) and (ticTac[0] == ticTac[6]) and ticTac[0] != "") or  ((ticTac[2] == ticTac[5]) and (ticTac[2] == ticTac[8]) and ticTac[2] != "" ) or  ((ticTac[2] == ticTac[4]) and (ticTac[2] == ticTac[6]) and ticTac[2] != "" ) or  ((ticTac[6] == ticTac[7]) and (ticTac[6] == ticTac[8]) and ticTac[6] != "" ) or  ((ticTac[3] == ticTac[4]) and (ticTac[3] == ticTac[5]) and ticTac[3] != "" ) or  ((ticTac[1] == ticTac[4]) and (ticTac[1] == ticTac[7]) and ticTac[1] != "" ):  
                     print("Player1 is the winner")
                     i = 10
-                    
+
                 i = i +1
             else:
                 print(value + " is the invalid value input")
