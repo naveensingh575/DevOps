a06dbbf67db035657446b550e9ee38091f891a5b
Naveen KUmar <naveenofficial210@gmail.com>
2024-12-27 15:56:19 +0530

    string.py

---
+++ Linux/shellSctpting/loop operator/whileLoop/no.sh
@@ -0,0 +1,16 @@
+#syntax
+#while condition is true
+# do
+#    statement
+# done
+#for e.g
+#!/usr/bin/bash
+echo "please enter the number"
+read number
+
+while [ $number -le 10 ]
+do
+    echo "$number"
+    number=$(( number+1 ))
+done
+echo "loop is completed"
