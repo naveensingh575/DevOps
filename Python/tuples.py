8eec20ceffeae225e3d4b4e7e3294682965ebe3c
Naveen KUmar <naveenofficial210@gmail.com>
2025-01-02 23:54:16 +0530

    tuples

---
+++ Python/tuples.py
@@ -0,0 +1,22 @@
+#we will read about tuple
+#tuple are immutable
+#tuple are inclosed in ()
+#tuple are collection of data types which inclosed in ( ) and seperated by , with ''
+#tuple are read only.
+#tuple are faster than list
+#tuple are used for fixed data
+#tuple are used for string formatting
+#tuple are used for function arguments
+#tuple are used for return multiple values from function
+#tuple are used for named tuple
+#tuple are used for immutable set
+#tuple are used for record in database
+#tuple are used for constant values
+#tuple are used for dictionary keys and values
+#tuple are used for dictionary items    
+print(dir(tuple))
+tuple = ('navi','singh','kumar','ji')
+print(tuple.index('navi'))
+print(tuple.index('kumar'))
+print(tuple.count('navi'))
+print(tuple.count(''))
